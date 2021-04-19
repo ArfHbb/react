9135723b0203a3a29c29e2213727743b4a8d61e7
ArfHbb <52080599+ArfHbb@users.noreply.github.com>
2021-04-19 22:48:03 +0700

    Add files via upload

---
+++ react_study_2/page7/Language.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Language extends React.Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div className='language-item'>
+        {/* Gantikan code dibawah agar mendisplay prop name */}
+        <div className='language-name'>
+        {this.props.name}
+        </div>
+        
+        {/* Gantikan nilai atribut src agar mendisplay prop image */}
+        <img 
+          className='language-image'
+          src={this.props.image}
+        />
+        
+      </div>
+    );
+  }
+}
+
+export default Language;
